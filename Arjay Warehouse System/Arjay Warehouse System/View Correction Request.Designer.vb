5082e375e5f53d44db239d04f671e4f6d533a20d
rockstartraders <32810135+rockstartraders@users.noreply.github.com>
2018-12-19 02:08:31 +0800

    completed View Correction Request

---
+++ Arjay Warehouse System/Arjay Warehouse System/View Correction Request.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class View_Correction_Request
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(255, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "View Correction Request"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(75, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 82
+        Me.Label2.Text = "Date Submitted"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(167, 531)
+        Me.TextBox8.Multiline = True
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(310, 113)
+        Me.TextBox8.TabIndex = 102
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(74, 534)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(81, 13)
+        Me.Label12.TabIndex = 101
+        Me.Label12.Text = "Admin Remarks"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(167, 387)
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ReadOnly = True
+        Me.TextBox7.Size = New System.Drawing.Size(310, 121)
+        Me.TextBox7.TabIndex = 100
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(64, 387)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(101, 13)
+        Me.Label11.TabIndex = 99
+        Me.Label11.Text = "Problem Description"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Enabled = False
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"", "Time Correction", "Employee Data Correction", "Login/Access Correction", "Inventory Correction (Receiving) ", "Inventory Correction (Dispatch)", "Others"})
+        Me.ComboBox2.Location = New System.Drawing.Point(167, 348)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(179, 21)
+        Me.ComboBox2.TabIndex = 98
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(64, 357)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(96, 13)
+        Me.Label10.TabIndex = 97
+        Me.Label10.Text = "Correction Needed"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Open", "Resolved"})
+        Me.ComboBox1.Location = New System.Drawing.Point(167, 315)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(179, 21)
+        Me.ComboBox1.TabIndex = 96
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(116, 323)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 13)
+        Me.Label9.TabIndex = 95
+        Me.Label9.Text = "Status"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(167, 281)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox6.TabIndex = 94
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(96, 288)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(62, 13)
+        Me.Label8.TabIndex = 93
+        Me.Label8.Text = "Department"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest
+        Me.TextBox5.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        Me.TextBox5.Location = New System.Drawing.Point(167, 246)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox5.TabIndex = 92
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(96, 253)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(58, 13)
+        Me.Label7.TabIndex = 91
+        Me.Label7.Text = "Last Name"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(167, 208)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox4.TabIndex = 90
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(85, 215)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(69, 13)
+        Me.Label6.TabIndex = 89
+        Me.Label6.Text = "Middle Name"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(167, 169)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox3.TabIndex = 88
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(96, 176)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 13)
+        Me.Label5.TabIndex = 87
+        Me.Label5.Text = "First Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(167, 129)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox2.TabIndex = 86
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(80, 129)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 13)
+        Me.Label4.TabIndex = 85
+        Me.Label4.Text = "Employee No."
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(167, 86)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox1.TabIndex = 84
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(80, 94)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(80, 13)
+        Me.Label13.TabIndex = 83
+        Me.Label13.Text = "INCIDENT NO."
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(167, 50)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.ReadOnly = True
+        Me.TextBox9.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox9.TabIndex = 103
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(247, 697)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 104
+        Me.Button1.Text = "CLOSE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(340, 697)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 105
+        Me.Button2.Text = "REFRESH"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'View_Correction_Request
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(663, 769)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "View_Correction_Request"
+        Me.Text = "View Correction Request"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ Arjay Warehouse System/Arjay Warehouse System/View Correction Request.vb
@@ -0,0 +1,171 @@
+﻿Imports System.Data.SqlClient
+Imports MySql.Data.MySqlClient
+Imports System.IO
+
+' Comments, Error and URL ' 
+' Timeouts = https://www.grasshopper3d.com/group/slingshot/forum/topics/problem-with-connection '
+' Out of range = https://stackoverflow.com/questions/14284494/mysql-error-1264-out-of-range-value-for-column '
+
+Public Class View_Correction_Request
+    Dim con As New MySqlConnection("Server=db4free.net;port=3306;userid=arjaywarehouse;password=Hulinghulingproject;database=arjay_warehouse;old guids=true;Connection Timeout=240;")
+    Dim cmd As MySqlCommand
+    Dim rd As MySqlDataReader
+    Dim query As String
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub View_Correction_Request_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+       
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        Dim con As New MySqlConnection("Server=db4free.net;port=3306;userid=arjaywarehouse;password=Hulinghulingproject;database=arjay_warehouse;old guids=true;Connection Timeout=240;")
+        Dim table As New DataTable()
+        Dim da As New SqlClient.SqlDataAdapter
+        Dim rd As MySqlDataReader
+
+        Try
+
+
+            con.Open()
+            Dim query As String
+            query = "SELECT * FROM `correction request` where `emp_no` ='" + TextBox2.Text + "'"
+            cmd = New MySqlCommand(query, con)
+            rd = cmd.ExecuteReader
+            While rd.Read
+
+                TextBox1.Text = rd.Item("Incident_no")
+                TextBox9.Text = rd.Item("date_submitted")
+                TextBox3.Text = rd.Item("f_name")
+                TextBox4.Text = rd.Item("m_name")
+                TextBox5.Text = rd.Item("l_name")
+                TextBox6.Text = rd.Item("dept")
+                ComboBox1.Text = rd.Item("status")
+                ComboBox2.Text = rd.Item("correction_type")
+                TextBox7.Text = rd.Item("prob_des")
+                TextBox8.Text = rd.Item("resolution")
+
+            End While
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+        Dim con As New MySqlConnection("Server=db4free.net;port=3306;userid=arjaywarehouse;password=Hulinghulingproject;database=arjay_warehouse;old guids=true;Connection Timeout=240;")
+        Dim table As New DataTable()
+        Dim da As New SqlClient.SqlDataAdapter
+        Dim rd As MySqlDataReader
+
+        Try
+
+
+            con.Open()
+            Dim query As String
+            query = "SELECT * FROM `correction request` where `Incident_no` ='" + TextBox1.Text + "'"
+            cmd = New MySqlCommand(query, con)
+            rd = cmd.ExecuteReader
+            While rd.Read
+
+                TextBox2.Text = rd.Item("emp_no")
+                TextBox9.Text = rd.Item("date_submitted")
+                TextBox3.Text = rd.Item("f_name")
+                TextBox4.Text = rd.Item("m_name")
+                TextBox5.Text = rd.Item("l_name")
+                TextBox6.Text = rd.Item("dept")
+                ComboBox1.Text = rd.Item("status")
+                ComboBox2.Text = rd.Item("correction_type")
+                TextBox7.Text = rd.Item("prob_des")
+                TextBox8.Text = rd.Item("resolution")
+
+            End While
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub TextBox8_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox8.TextChanged
+
+    End Sub
+
+    Private Sub TextBox9_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox9.TextChanged
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox5.TextChanged
+        Dim con As New MySqlConnection("Server=db4free.net;port=3306;userid=arjaywarehouse;password=Hulinghulingproject;database=arjay_warehouse;old guids=true;Connection Timeout=240;")
+        Dim table As New DataTable()
+        Dim da As New SqlClient.SqlDataAdapter
+        Dim rd As MySqlDataReader
+
+        Try
+
+
+            con.Open()
+            Dim query As String
+            query = "SELECT * FROM `correction request` where `l_name` ='" + TextBox5.Text + "'"
+            cmd = New MySqlCommand(query, con)
+            rd = cmd.ExecuteReader
+            While rd.Read
+
+                TextBox1.Text = rd.Item("Incident_no")
+                TextBox9.Text = rd.Item("date_submitted")
+                TextBox3.Text = rd.Item("f_name")
+                TextBox4.Text = rd.Item("m_name")
+                TextBox2.Text = rd.Item("emp_no")
+                TextBox6.Text = rd.Item("dept")
+                ComboBox1.Text = rd.Item("status")
+                ComboBox2.Text = rd.Item("correction_type")
+                TextBox7.Text = rd.Item("prob_des")
+                TextBox8.Text = rd.Item("resolution")
+
+
+
+            End While
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+        Me.Dispose()
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        '<------  clear all fields ---->
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox9.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+
+    End Sub
+End Class
