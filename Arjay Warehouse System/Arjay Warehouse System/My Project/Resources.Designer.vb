fca0a16b06b6af661109c0c41d999a65f75d5af1
rockstartraders <32810135+rockstartraders@users.noreply.github.com>
2019-01-01 23:07:10 +0800

    Downloaded Images and GIF

--- Arjay Warehouse System/Arjay Warehouse System/My Project/Resources.Designer.vb
+++ Arjay Warehouse System/Arjay Warehouse System/My Project/Resources.Designer.vb
@@ -69,5 +69,35 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property Rocket() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Rocket", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property team_rocket() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("team_rocket", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property team_rocket1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("team_rocket1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
